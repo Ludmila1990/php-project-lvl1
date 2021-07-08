933a9465a22c17cea5e5601ae5cfc3208f7831af
Lusya1990 <lusya_suntsova1990@mail.ru>
2021-07-08 18:25:43 +0500

    files changed

--- src/Cli.php
+++ src/Cli.php
@@ -42,3 +42,47 @@ Let's try again, {$name}!");
         }
     }
 }
+
+function calculate()
+{
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('What is the result of the expression?');
+    
+    $i = 1;
+    $res = [];
+    while ($i <= 3) {   
+        $num1 = rand(1, 20);
+        $num2 = rand(1, 20);
+        $operations = ['+', '-', '*'];
+        $operator = $operations[array_rand($operations, 1)];
+        $expression = "{$num1} {$operator} {$num2}";
+        $question = line('Question: %s', $expression);
+        $answer = prompt('Your answer');
+        switch ($operator) {
+            case '+':
+                $result = $num1 + $num2;
+                break;
+            case '-':
+                $result = $num1 - $num2;
+                break;
+            case '*':
+                $result = $num1 * $num2;
+                break;
+        }
+        if ((int) $answer === $result) {
+        line('Correct!'); 
+        $i++;
+        $res[] = $answer;
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was '{$result}'.
+Let's try again, {$name}!");
+            break;
+        }
+        if (count($res) === 3) {
+            line("Congratulations, {$name}!");
+        }
+    }
+}
+
