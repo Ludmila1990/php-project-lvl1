01a5de22cce43709993b2620a17c76cbf3ab2858
Lusya1990 <lusya_suntsova1990@mail.ru>
2021-07-21 01:00:23 +0500

    new file

---
+++ src/games/Calc.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace BrainGames\games\Calc;
+
+use function BrainGames\Engine\run;
+
+const DESCRIPTION = "What is the result of the expression?";
+
+function play()
+{
+    $runGame = function () {
+        $num1 = rand(1, 20);
+        $num2 = rand(1, 20);
+        $operations = ['+', '-', '*'];
+        $operator = $operations[array_rand($operations, 1)];
+        $question = "{$num1} {$operator} {$num2}";
+        $correctAnswer = 0;
+        switch ($operator) {
+            case '+':
+                $correctAnswer = $num1 + $num2;
+                break;
+            case '-':
+                $correctAnswer = $num1 - $num2;
+                break;
+            case '*':
+                $correctAnswer = $num1 * $num2;
+                break;
+        }
+        return [$question, (string) $correctAnswer];
+    };
+    run(DESCRIPTION, $runGame);
+}
+
+?>
