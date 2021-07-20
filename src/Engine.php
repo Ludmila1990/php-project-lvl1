ffb516f91a2d31a105eba88a3f2e39f9129d1a10
Lusya1990 <lusya_suntsova1990@mail.ru>
2021-07-21 00:27:32 +0500

    new file

---
+++ src/Engine.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BrainGames\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function run($description, $runGame, $countRound = 3)
+{
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line($description);
+    
+    for ($i = 1; $i <= $countRound; $i++) {
+        [$question, $correctAnswer] = $runGame();
+        $questionMessage = line("Question: %s", $question);
+        $answerUser = prompt('Your answer');
+        if ($answerUser === $correctAnswer) {
+            line('Correct!');
+        }else {
+            line("'{$answerUser}' is wrong answer ;(. Correct answer was '{$correctAnswer}'. 
+Let's try again, {$name}!");
+            return;
+        }
+    }
+    line("Congratulations, {$name}!");
+}
+?>
