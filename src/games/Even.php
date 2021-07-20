ff9036da87e96b91cef656eb5c22cd2526464ed2
Lusya1990 <lusya_suntsova1990@mail.ru>
2021-07-21 00:28:02 +0500

    new file

---
+++ src/games/Even.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BrainGames\games\Even;
+
+use function BrainGames\Engine\run;
+
+const DESCRIPTION = "Answer 'yes' if the number is even, otherwise answer 'no'.";
+
+function isEven($num)
+{
+    return $num % 2 === 0;
+}
+
+function play()
+{
+    $runGame = function () {
+        $question = rand(1, 20);
+        $correctAnswer = isEven($question) ? 'yes' : 'no';
+        return [$question, $correctAnswer];
+    };
+    run(DESCRIPTION, $runGame);
+}
+
+
+?>
