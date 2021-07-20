be7b94e348c8811eaea3472375ea36681198affd
Lusya1990 <lusya_suntsova1990@mail.ru>
2021-07-21 01:22:00 +0500

    new file

---
+++ src/games/Gcd.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BrainGames\games\Gcd;
+
+use function BrainGames\Engine\run;
+
+const DESCRIPTION = "Find the greatest common divisor of given numbers.";
+
+function findGcd($a, $b)
+{
+    $large = $a > $b ? $a : $b;
+    $small = $a > $b ? $b : $a;
+    $remainder = $large % $small;
+    return $remainder == 0 ? $small : findGcd($small, $remainder);
+}
+
+function play()
+{
+    $runGame = function () {
+        $num1 = rand(1, 100);
+        $num2 = rand(1, 100);
+        $question = "{$num1} {$num2}";
+        $correctAnswer = findGcd($num1, $num2); 
+        return [$question, (string) $correctAnswer];
+    };
+    run(DESCRIPTION, $runGame);
+}
+
+?>
