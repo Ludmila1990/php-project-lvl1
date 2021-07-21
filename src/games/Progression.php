b0cd20f2bd554e7825163e58076a49627a6184f2
Lusya1990 <lusya_suntsova1990@mail.ru>
2021-07-22 01:12:58 +0500

    new file

---
+++ src/games/Progression.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace BrainGames\games\Progression;
+
+use function BrainGames\Engine\run;
+
+const DESCRIPTION = "What number is missing in the progression?";
+
+function getTheProgression($num) {
+    $step = rand(1, 10);
+    $res = [];
+    $endNum = $num + 9 * $step;
+    for ($i = $num; $i <= $endNum; $i += $step) {
+      $res[] = $i;
+    }
+    $keyRand = rand(0, 9);
+    $newValue = '..';
+    $result = [];
+    $findValue = '';
+    foreach ($res as $key => $value) {
+      if ($key === $keyRand) {
+        $findValue = $res[$key];
+        $result[$key] = $newValue;
+      }else {
+        $result[] = $value;
+      }
+    }
+   $string = implode(' ', $result);
+   return [$string, $findValue];
+}
+
+function play()
+{
+    $runGame = function () {
+        $progression = getTheProgression(rand(1, 100));
+        $question = $progression[0];
+        $correctAnswer = (string) $progression[1];
+        return [$question, $correctAnswer];
+    };
+    run(DESCRIPTION, $runGame);
+}
+
+?>
