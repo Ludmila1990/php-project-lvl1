838c9c08ef1798749f83d707482c2f5208f39e6f
Lusya1990 <lusya_suntsova1990@mail.ru>
2021-07-23 01:01:45 +0500

    new file

---
+++ src/games/Prime.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace BrainGames\games\Prime;
+
+use function BrainGames\Engine\run;
+
+const DESCRIPTION = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+
+function isPrime($num)
+{
+    for ($i = 2; $i < $num; $i++) {
+        if ($num % $i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function play()
+{
+    $runGame = function () {
+        $question = rand(1, 20);
+        $correctAnswer = isPrime($question) ? 'yes' : 'no';
+        return [$question, $correctAnswer];
+    };
+    run(DESCRIPTION, $runGame);
+}
