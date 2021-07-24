5fa50269c2f3b63c6b6c5e8fadd5a4c11aeb4cc4
Lusya1990 <lusya_suntsova1990@mail.ru>
2021-07-24 15:16:09 +0500

    changed file

--- src/games/Prime.php
+++ src/games/Prime.php
@@ -8,6 +8,9 @@ const DESCRIPTION = "Answer 'yes' if given number is prime. Otherwise answer 'no
 
 function isPrime($num)
 {
+    if ($num === 1) {
+        return false;
+    }
     for ($i = 2; $i < $num; $i++) {
         if ($num % $i === 0) {
             return false;
